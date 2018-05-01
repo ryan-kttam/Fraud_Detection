19e6d9c05f915c90e0efceb9341445b6e9255200
ryan-kttam <ryantam0626@gmail.com>
2018-04-30 22:27:33 -0700

    added graph illustration and split the data into training & valid & test sets

--- fraud_detection.py
+++ fraud_detection.py
@@ -20,3 +20,33 @@ Counter(dataset['Class'])
 
 # there are less than 0.2% cases that are fraud
 len(dataset[dataset['Class'] == 1])* 100.0 / len(dataset)
+
+# select all features except the response variable
+# using iloc: [row, col], here, I selected all rows, and 0:-1 columns
+features = dataset.iloc[:, 0:-1]
+response = dataset['Class']
+
+# explore features' relationship of each other
+# first 5 features
+# V1 has a positive relationship between V2 and V3, while a negative relationship with V4
+# all V1 to V4 have a very sharp density at 0
+pd.scatter_matrix(features.iloc[: , 0:5], alpha = 0.3, figsize = (14,8), diagonal = 'kde');
+
+
+# separating the data into training set, validation set, and test set
+from sklearn.model_selection import train_test_split
+
+trainDataX, testDataX,  trainDataY, testDataY = train_test_split( features,
+                                                                  response,
+                                                                  test_size=0.1,
+                                                                  random_state=1)
+
+modelDataX, validDataX,  modelDataY, validDataY = train_test_split( trainDataX,
+                                                                    trainDataY,
+                                                                    test_size=0.25,
+                                                                    random_state=1)
+
+trainDataX.shape
+testDataX.shape
+modelDataX.shape
+validDataX.shape
